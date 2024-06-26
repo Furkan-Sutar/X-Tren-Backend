d617bbc6e6bcc860372f1c0cc955311418df9eb9
Furkansutar <154055220+Furkansutar@users.noreply.github.com>
2024-06-26 11:36:46 +0530

    contact messege update function

--- router/ContactRouter.js
+++ router/ContactRouter.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const router = express.Router();
 const ContactForm = require('../models/ContactForm');
@@ -5,20 +6,19 @@ const ContactForm = require('../models/ContactForm');
 router.post('/contact', async (req, res) => {
   try {
     const { username, email, message } = req.body;
-                                                  
-    const form = new ContactForm({
-      username,
-      email,
-      message,
-    });
 
-const saveData = await form.save()    
-    console.log('saveData', saveData);
+    // Find and update the document if email and username exist, otherwise create a new one
+    const form = await ContactForm.findOneAndUpdate(
+      { email, username },
+      { message, date: Date.now() },
+      { new: true, upsert: true, setDefaultsOnInsert: true }
+    );
+
     res.status(201).json({
       message: 'Contact form submitted successfully',
       error: false,
       success: true,
-      data: saveData,
+      data: form,
     });
   } catch (error) {
     console.error('Error saving contact form:', error);
